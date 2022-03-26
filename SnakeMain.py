80cac3c94788545cd44ec8fe31fdcf4a37bfbc96
Tesfay-Hagos <tesfay.hagos1421@gmail.com>
2022-03-26 08:14:17 +0500

    one food clas is added to generate random food to the screen

--- SnakeMain.py
+++ SnakeMain.py
@@ -1,6 +1,9 @@
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 from turtle import Turtle, Screen
-
+DOWN = 270
+UP = 90
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     screen = Screen()
@@ -9,6 +12,7 @@ class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake()
+        self.head = self.snake[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -18,9 +22,26 @@ class Snake:
             ob.goto(position)
             self.snake.append(ob)
 
+    def up(self):
+
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def left(self):
+
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
     def move(self):
         for seq_num in range(len(self.snake) - 1, 0, -1):
             new_x = self.snake[seq_num - 1].xcor()
             new_y = self.snake[seq_num - 1].ycor()
             self.snake[seq_num].goto(new_x, new_y)
-        self.snake[0].forward(20)
+        self.head.forward(20)
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 
-import SnakeMain
+from SnakeMain import Snake
+from food import Food
 
 screen= Screen()
 screen.setup(width=600, height=600, startx=-300, starty=0)
 screen.bgcolor("black")
 screen.title("Snake_game_by_TesfayH")
 
-snake = SnakeMain.Snake()
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on=True
 while game_is_on:
     screen.update()
